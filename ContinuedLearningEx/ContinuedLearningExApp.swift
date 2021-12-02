d088929d9e15c7a22d18c49c49914e8c0fcb0450
kwonilgun <kwonilgun@naver.com>
2021-12-03 08:35:40 +0900

    Initial Commit

---
+++ ContinuedLearningEx/ContinuedLearningExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningExApp.swift
+//  ContinuedLearningEx
+//
+//  Created by kwonilgun on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
